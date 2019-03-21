d64b35270322917da68bdb713461e68d72004d8a
JoeZufelt <joe.zufelt@gmail.com>
2019-03-21 19:21:48 -0400

    HAHAHAHAHAHAHAHAHAHA *upside down smileyface

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,18 +1,18 @@
 // This is middleware for restricting routes a user is not allowed to visit if not logged in
-// module.exports = function(req, res, next) {
-//   // If the user is logged in, continue with the request to the restricted route
-//   if (req.user) {
-//     return next();
-//   }
+module.exports = function(req, res, next) {
+  // If the user is logged in, continue with the request to the restricted route
+  if (req.user) {
+    return next();
+  }
 
-//   // If the user isn't logged in, redirect them to the login page
-//   return res.redirect("/");
-// };
-var session = require("express-session"),
-  bodyParser = require("body-parser");
+  // If the user isn't logged in, redirect them to the login page
+  return res.redirect("/");
+};
+// var session = require("express-session"),
+//   bodyParser = require("body-parser");
 
-app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(express.static("public"));
+// app.use(session({ secret: "cats" }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(passport.initialize());
+// app.use(passport.session());
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 
 
-
-
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     firstname: { 
