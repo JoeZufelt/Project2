2f1b9ab1865e03cfe541447e66428f34f60ec5d8
JoeZufelt <joe.zufelt@gmail.com>
2019-03-21 21:29:46 -0400

    changed user.js

--- models/user.js
+++ models/user.js
@@ -1,81 +1,19 @@
 var bcrypt = require("bcrypt-nodejs");
 
 module.exports = function(sequelize, DataTypes) {
-//   var User = sequelize.define("User", {
-//     firstname: { 
-//       type: DataTypes.STRING,
-//       /*allowNull: false,*/
 
-//       validate: {
-//         len:[1,15],
-//         notEmpty: true,
-//         isAlphanumeric: true,
-//       }
-//     },
-//     lastname: {
-//       type: DataTypes.STRING,
-
-//       /*allowNull: false,*/
-
-//       validate: {
-//         /*len:[0,20]*/
-//         notEmpty: true,
-//       }
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-
-//     /*allowNull: false,*/
-
-//     validate: {
-//       notEmpty: true,
-//       isEmail: true,
-//     }
-    
-//   },
-
-//   zipcode: {
-
-//     type: DataTypes.INTEGER,
-
-
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("User", {
-    name: { 
-      type: DataTypes.STRING,
-      /*allowNull: false,*/
-
-      validate: {
-        len:[1,15],
-        notEmpty: true,
-        isAlphanumeric: true,
-      }
-    },
-  email: {
-    type: DataTypes.STRING,
-
-//     validate: {
-//       /*min:1,
-//       max:5,*/
-//       notEmpty: true,
-//       isNumeric: true,    
-//     }
-
-//   },
-//   description: {
-//     type: DataTypes.TEXT,
-
-//     validate: {
-//       notEmpty: true
-//     }
-//   }
-
-
-
-
-// });
 var User = sequelize.define("User", {
   // The email cannot be null, and must be a proper email before creation
+  name: { 
+    type: DataTypes.STRING,
+    /*allowNull: false,*/
+
+    validate: {
+      len:[1,15],
+      notEmpty: true,
+      isAlphanumeric: true,
+    }
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
